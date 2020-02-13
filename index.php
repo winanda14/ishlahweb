dca8415f54225f8a1ca9a24c9f15be31fbece499
rachman996 <m.refaldorachman@gmail.com>
2020-02-13 10:30:48 +0700

    mengubah isi text

---
+++ index.php
@@ -0,0 +1,414 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <!-- javascript -->
+    <link rel="stylesheet" href="js/script.js" >
+    
+    <!-- font awesome -->
+    <link rel="stylesheet" href="css/all.css">
+    <!-- link owl carousel -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">    
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,400,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>LDK ISHLAH Y.A.I</title>
+
+    <style>
+      
+    </style>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+    <section class="navbar">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+      <div class="container">
+  <a class="navbar-brand" href="#"><img src="img/logo.png" alt="ishlah  "></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item active">
+        <a class="nav-link " href="#" >Beranda <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Artikel</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Struktur</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gallery</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+</section>
+    <!-- akhir navbar -->
+
+
+    <!--awal carousel -->
+    
+    <section>
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/image.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-block pb-5" >
+        <h5>SELAMAT DATANG</h5>
+        <h5>Di Website LDK ISHLAH Y.A.I</h5>
+        <a href="btn" class="btn  btn-success bg-success text-white">Gabung</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="img/image.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-block pb-5" >
+        <h5>SELAMAT DATANG</h5>
+        <h5>Di Website LDK ISHLAH Y.A.I</h5>
+        <a href="btn" class="btn  btn-success bg-success text-white">Gabung</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="img/image.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-block pb-5">
+        <h5>SELAMAT DATANG</h5>
+        <h5>Di Website LDK ISHLAH Y.A.I</span></h5>
+        <a href="btn" class="btn  btn-success bg-success text-white">Gabung</a>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </section>
+
+
+    <!-- akhir carousel -->
+
+    <!-- test -->
+    
+    <!-- awal Content visi-->
+
+
+    <div class="container text-center mt-5 mb-5 fluid">
+      <h1 class="visi pb-4">VISI </h1>
+      <p class="text-justify" style="font-weight: 700;">" Terbentuk dan terwujudnya mahasiswa, karyawan dan segenap civitas akademika LPT Y.A.I yang memiliki keunggulan, moralitas islam, intelektualitas, humanitas dan profesionalitas sehingga terwujudnya peradaban islam " </p>
+
+    </div>
+    <!-- akhir visi -->
+    
+    
+    
+    <!-- awal program kerja -->
+    <section class="proker ">
+    <div class="programkerja pb-5">
+    <div class="container text-center ">
+    <h1 class="pt-5 pb-4" style="margin-top: 100px;">PROGRAM KERJA</h1>
+    <div class="row mt-3 pt-3">
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/6.png" alt=""class=" figure-img img-fluid"></a>
+          <fofigurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/6.png " alt=""class=" figure-img img-fluid"></a>
+          <figurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/2.png " alt=""class=" figure-img img-fluid"></a>
+          <figurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/2.png " alt=""class=" figure-img img-fluid"></a>
+          <figurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+    </div>
+    <div class="row mt-3 pt-3">
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/6.png" alt=""class=" figure-img img-fluid"></a>
+          <fofigurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/6.png " alt=""class=" figure-img img-fluid"></a>
+          <fofigurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/2.png " alt=""class=" figure-img img-fluid"></a>
+          <fofigurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+      <div class="col-sm-12 col-md-6 col-lg-3">
+        <figure class="figure ">
+          <a href=""> <img src="img/2.png " alt=""class=" figure-img img-fluid"></a>
+          <figurecaption class="figure-caption"> 
+        <h5>Pengabul</h5>   
+        </figurecaption>
+        </figure>
+      </div>
+    </div>
+    </div>
+    </div>
+  
+</div>
+</div>
+</section>
+
+    <!-- akhir program kerja -->
+
+    <!-- statement -->
+
+<div class="slider-area">
+  <!-- <h1>Apa kata mereka</h1>   -->
+<div class="container">
+  
+<div id="carouselExampleCaptions1" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions1" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions1" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions1" data-slide-to="2"></li>
+  </ol>
+  
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+
+    <div class="img-area ">
+    
+    <img src="img/6.png" alt="" class="">
+    </div>
+          
+      <div class="carousel-caption d-none d-block row " id="caption">
+      <h1 style="position:absolute; top:0; ; " class="text-center float-fluid  w-300 pb-3"><span class="col-auto test1 " style="margin-left: 430px">apa kata mereka?</span></h1>
+      <p style="position:absolute; top:0; margin-left: 10px; margin-top:100px; font-weight:200 font-size:20px;" class="text-center ">
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor, numquam quae nemo fuga consectetur ullam exercitationem officia sit tempore esse aliquid recusandae quod natus ipsum odio nihil ut inventore?"</p>
+      <h5 class="font-weight-bold"><span>Ahmad</span></h5>
+        <p>Ketua umum ishlah 2012</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <div class="img-area">
+    <img src="img/6.png" alt="">
+    </div>
+      
+      <div class="carousel-caption d-none d-block" id="caption">
+      <h1 style="position:absolute; top:0; margin-left: 430px; " class="text-center "><span class="test1 mt-5">apa kata mereka?</span></h1>
+      <p style="position:absolute; top:0; margin-left: 10px; margin-top:100px; font-weight:200 font-size:20px;" class="text-center ">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor, numquam quae nemo fuga consectetur ullam exercitationem officia sit tempore esse aliquid recusandae quod natus ipsum odio nihil ut inventore?"</p>
+      <h5 class="font-weight-bold"><span>Ahmad</span></h5>
+        <p>Ketua umum ishlah 2011</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <div class="img-area">
+    <img src="img/6.png" alt="">
+    </div>
+      
+      <div class="carousel-caption d-none d-block" id="caption">
+      <h1 style="position:absolute; top:0; margin-left: 430px; " class="text-center "><span class="test1 mt-5">apa kata mereka?</span></h1>
+      <p style="position:absolute; top:0; margin-left: 10px; margin-top:100px; font-weight:200 font-size:20px;" class="text-center ">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor, numquam quae nemo fuga consectetur ullam exercitationem officia sit tempore esse aliquid recusandae quod natus ipsum odio nihil ut inventore?"</p>
+      <h5 class="font-weight-bold"><span>Ahmad</span></h5>
+        <p class="">Ketua umum ishlah 2011</p>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions1" role="button" data-slide="prev" style="    background: transparent;
+">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions1" role="button" data-slide="next"    style=" background: transparent;"
+>
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</div>
+
+</div>
+
+
+    <!-- akhir statement -->
+
+  <!-- awal artikel -->
+<section class="artikel1 pb-5 mb-5">
+<div class="container "> 
+  <div class="row pt-5 pb-4">
+    <div class="col font-weight-bold text-center">
+      <h1>ARTIKEL</h1>
+    </div>
+  </div>
+<div class="row">
+  <div class="col-sm-12 col-md-12 col-lg-4">
+<div class="card" >
+  
+  <img class="card-img-top" src="img/masjid.png" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">"One day one hadist : Keutamaan 
+baca Al-Qur'an"</p>
+    </div>
+    </div>
+    </div>
+    <div class="col-sm-12 col-md-12 col-lg-4">
+<div class="card" >
+  
+  <img class="card-img-top" src="img/masjid.png" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">"One day one hadist : Keutamaan 
+baca Al-Qur'an"</p>
+    </div>
+    </div>
+    </div>
+  
+    <div class="col-sm-12 col-md-12 col-lg-4">
+<div class="card" >
+  
+  <img class="card-img-top" src="img/masjid.png" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">"One day one hadist : Keutamaan 
+baca Al-Qur'an"</p>
+    </div>
+    </div>
+    </div>
+  
+    </div>  
+</div>
+</section>
+
+
+  <!-- akhir artikel -->
+
+
+
+<!-- awal tentang -->
+<section class="video">
+<div class="container pt-5 pb-5">
+
+<div class="row">
+  <div class="col-sm-12 col-md-12 col-lg-6">
+    <h1>TENTANG LDK ISHLAH</h1>
+    <p class="text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam iure magnam a expedita tempore. Repellat nostrum ipsa officiis nobis ducimus, sed debitis ut quas alias sequi temporibus amet. Quisquam, est. Ratione alias dignissimos quod, sequi inventore velit, libero voluptatibus voluptas laboriosam quas molestias! Blanditiis quibusdam, sint fugiat iure debitis laboriosam.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tempore explicabo tenetur voluptate ullam nihil suscipit non id aliquid quos. A laboriosam laborum dignissimos voluptates ex, quidem nulla at alias?</p>
+
+  </div>
+  <div class="col-sm-12 col-md-12 col-lg-6">
+    <p><span class="videocapt"> Video Profile Ishlah<span></p>
+    
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/ke28sLoHNH0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class=""></iframe>
+  </div>
+</div>
+</div>
+
+
+</section>
+<!-- akhir tentang -->
+
+<!-- footer -->
+<footer class="footer"> 
+
+<div class="container">
+<div class="row">
+<div class="col-lg-5 col-md-5 pt-3">
+<h1 class="font-weight-bold">Sekretariat</>
+<p>Jl. Diponegoro No. 74 Jakarta Pusat, 
+Indonesia </p>
+</div>
+<div class="col-lg-5 col-md-5  font-weight-bold pt-4">
+<p >Copyright 2020 by Sistem Informasi Y.A.I</p>
+</div>
+<div class="col  pt-4  ">
+    <img src="img/fb.png" alt="">
+    <img src="img/ig.png" alt="">
+    <img src="img/twitter.png" alt="">
+</div>
+</div>
+</div>
+
+
+</footer
+
+<!-- akhir footer -->
+
+
+
+<!-- test -->
+<!-- test -->
+
+
+    <!-- Optional JavaScript -->
+   
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.4.1.min.js" ></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js" ></script>
+    <script src="js/all.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script> 
+<script>
+    $('.carousel').carousel({
+    interval: 3000,
+    pause: 'hover',
+    wrap: true,
+    touch: true
+});
+
+$(function(){
+  $("#customers-testimonials").owlCarousel({
+    items:1,
+    loop:true,  
+    autoplay:true,
+    autoplayTimeout:1000,
+    
+    look:true,
+    autoPlayHoverPause:true,
+    
+  });
+}); 
+
+</script>
+
+
+  </body>
+</html>
