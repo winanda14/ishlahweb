c3602ebf027a7edf37b1674c3baf6d36b3c35a86
rachman996 <m.refaldorachman@gmail.com>
2020-02-18 16:51:00 +0700

    menambahkan profile.php

---
+++ profile.php
@@ -0,0 +1,311 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <!-- javascript -->
+    <link rel="stylesheet" href="js/script.js" >
+    
+    <!-- font awesome -->
+    <link rel="stylesheet" href="css/all.css">
+    <!-- link owl carousel -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">    
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,400,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/logo.png">
+    <title>  LDK ISHLAH Y.A.I</title>
+
+    <style>
+      
+    </style>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+    <section class="navbar">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+      <div class="container">
+  <a class="navbar-brand" href="#" data-toggle="tooltip" data-placement="bottom" title="logo.png">
+  <img src="img/logo.png" alt="ishlah  " ></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item active">
+        <a class="nav-link " href="#" >Beranda <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Artikel</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Struktur</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gallery</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+</section>
+    <!-- akhir navbar -->
+
+
+    <!--awal carousel -->
+    
+    <section>
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/image.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-block pb-5" >
+        <h5>SELAMAT DATANG</h5>
+        <h5>Di Website LDK ISHLAH Y.A.I</h5>
+        <a href="btn" class="btn  btn-success bg-success text-white">Gabung</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="img/image.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-block pb-5" >
+        <h5>SELAMAT DATANG</h5>
+        <h5>Di Website LDK ISHLAH Y.A.I</h5>
+        <a href="btn" class="btn  btn-success bg-success text-white">Gabung</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <img src="img/image.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-block pb-5">
+        <h5>SELAMAT DATANG</h5>
+        <h5>Di Website LDK ISHLAH Y.A.I</span></h5>
+        <a href="btn" class="btn  btn-success bg-success text-white">Gabung</a>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </section>
+
+
+    <!-- akhir carousel -->
+
+    <!-- test -->
+    <!-- profile -->
+    <div class="container">
+<h1 class="text-center font-weight-bold">
+    Profile
+
+</h1>
+<h2 class="font-weight-bold"    >
+    sejarah
+</h2> 
+<p>
+    UKM Ishlah LPT YAI diresmikan atau dideklarasi sebagai lembaga formal intra Universitas pada hari rabu tanggal 1 agustus 2001, yang sebelumnya bernama Rohani Islam LPT YAI.
+    
+    
+</p>
+<p>
+    UKM Ishlah LPT YAI bertempat dan berkedudukan di kampus LPT YAI
+    Lambang</p>
+
+   
+      <h5 class="font-weight-bold"> Lambang</h5>
+      <p class="mb-3">
+        Lambang UKM Ishlah
+    </p>
+    <p>
+        A. Kubah : Melambangkan suatu bangunan yang berfungsi menaungi seluruh mahasiswa/i YAi dalam membangun dan memelihara ukhuwah Islamiyah yang utuh.
+    <br>
+        B. Bulan-Bintang : Melambangkan tingginya nilai nilai islam yang menjadi inti dan pedoman bagi segenap aktivitas UKM Ishlah LPT YAI 
+    <br>
+       C. Buku : Melambangkan sifat dan keperibadian mahasiswa/i Islam LPT YAI yang senantiasa mempelajari ilmu dunia dan ilmu akhirat dalam membentuk masyarakat indonesai seutuhnya
+    </p>
+    <p>
+        Warna Hijau : Melambangkang keharmonisan, keseimbangan dan kedamaian yang mempresentasikan hubungan baik individu terhadap tuhanya dan makhluk ciptaan Nya, serta merupakan warna yang di sukai oleh rasulullah shollahu 'alaihi wasallam.
+    </p><hr>
+</div>
+
+
+    <!-- akhir profil -->
+
+  <!-- awal artikel -->
+<section class="artikel1 pb-5 mb-5">
+<div class="container "> 
+  <div class="row pt-5 pb-4">
+    <div class="col font-weight-bold text-center">
+      <h1>ARTIKEL</h1>
+    </div>
+  </div>
+<div class="row">
+  <div class="col-sm-12 col-md-12 col-lg-4">
+<div class="card" >
+  
+  <img class="card-img-top" src="img/masjid.png" alt="Card image cap">
+  <div class="card-body">
+  <a href=""><p class="card-text">"One day one hadist : Keutamaan 
+baca Al-Qur'an"</p></a>
+    </div>
+    </div>
+    </div>
+    <div class="col-sm-12 col-md-12 col-lg-4">
+<div class="card" >
+  
+  <img class="card-img-top" src="img/masjid.png" alt="Card image cap">
+  <div class="card-body">
+    <a href=""><p class="card-text">"One day one hadist : Keutamaan 
+baca Al-Qur'an"</p></a>
+    </div>
+    </div>
+    </div>
+  
+    <div class="col-sm-12 col-md-12 col-lg-4">
+<div class="card" >
+  
+  <img class="card-img-top" src="img/masjid.png" alt="Card image cap">
+  <div class="card-body">
+  <a href=""> <p class="card-text">"One day one hadist : Keutamaan 
+baca Al-Qur'an"</p></a>
+    </div>
+    </div>
+    </div>
+  
+    </div>  
+</div>
+</section>
+
+
+  <!-- akhir artikel -->
+
+
+
+<!-- awal tentang -->
+<section class="video">
+<div class="container pt-5 pb-5">
+
+<div class="row">
+  <div class="col-12 ">
+    <h1 class="text-center ">TENTANG LDK ISHLAH</h1>
+    <p class="text-justify pt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam iure magnam a expedita tempore. Repellat nostrum ipsa officiis nobis ducimus, sed debitis ut quas alias sequi temporibus amet. Quisquam, est. Ratione alias dignissimos quod, sequi inventore velit, libero voluptatibus voluptas laboriosam quas molestias! Blanditiis quibusdam, sint fugiat iure debitis laboriosam.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tempore explicabo tenetur voluptate ullam nihil suscipit non id aliquid quos. A laboriosam laborum dignissimos voluptates ex, quidem nulla at alias?</p>
+
+  </div>
+  </div>
+  <div class="row ">
+  <div class="pemutar col-12 ">
+    <p><span class="videocapt pt-2 "> Video Profile Ishlah<span></p>
+    
+  <iframe width="100%  " height="600" src="https://www.youtube.com/embed/ke28sLoHNH0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="" ></iframe>
+  </div>
+</div>
+</div>
+
+
+</section>
+<!-- akhir tentang -->
+
+<!-- footer -->
+<footer class="footer"> 
+
+<div class="container">
+<div class="row">
+<div class="col">
+<p class="moto pt-2">"Together Be Better" </p>
+<hr/ class="garis">
+</div>
+</div>
+<div class="row pb-5">
+<div class="col-4 pt-2">
+
+
+<h1 class="font-weight-bold   ">Sekretariat</>
+<p>Jl. Diponegoro No. 74 Jakarta Pusat, 
+Indonesia </p>
+
+</div>
+
+<div class="col-5 offset-3 text-right    pt-5">
+    <a href="">
+      <img src="img/fb.png" alt="">
+    </a>
+    <a href="">
+      <img src="img/ig.png" alt="">
+    </a>
+    <a href="">
+      <img src="img/twitter.png" alt="">
+    </a>
+</div>
+
+</div>
+</div>
+  </footer>
+
+<div class="subfooter row">
+
+<div class="col text-center">
+  <p>Copyright <i class="far fa-copyright"></i> 2020 by Sistem Informasi Y.A.I</p>
+</div>
+</div>
+
+<!-- akhir footer -->
+
+
+
+<!-- test -->
+<!-- test -->
+
+
+    <!-- Optional JavaScript -->
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.4.1.min.js" ></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js" ></script>
+    <script src="js/all.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script> 
+<script>
+    $('.carousel').carousel({
+    interval: 3000,
+    pause: 'hover',
+    wrap: true,
+    touch: true
+});
+
+$(function(){
+  $("#customers-testimonials").owlCarousel({
+    items:1,
+    loop:true,  
+    autoplay:true,
+    autoplayTimeout:1000,
+    
+    look:true,
+    autoPlayHoverPause:true,
+    
+  });
+}); 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+</script>
+
+
+  </body>
+</html>
